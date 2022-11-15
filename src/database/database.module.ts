72f758b412ff2d76eeaa35dca7cd3c15fa7a5d13
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-11-15 20:24:55 +0200

    create user entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import User from 'src/users/user.entity';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
@@ -11,7 +12,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/*.entity.ts'],
+        entities: [User],
         synchronize: true,
       }),
     }),
